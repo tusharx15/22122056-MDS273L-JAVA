9acf39705fd4c67e4528efeda2b683ada0b5ac6d
tusharx15 <118044440+tusharx15@users.noreply.github.com>
2023-02-06 14:42:32 +0530

    learning commit

--- Lab 2/lab2.java
+++ Lab 2/lab2.java
@@ -37,8 +37,6 @@ class NameSearch {
                         }
                         
                     }
-                    
-                    
                     break;
 
                 }
